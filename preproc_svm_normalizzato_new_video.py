8c81caf8874513aa2debec2cf97ebe412f8776a9
rmenoli <30891698+rmenoli@users.noreply.github.com>
2017-12-13 15:59:56 +0100

    viedo test

--- preproc_svm_normalizzato_new_video.py
+++ preproc_svm_normalizzato_new_video.py
@@ -22,7 +22,7 @@ listear=list(dati.ear)
 
 #normalizzo
 listear=np.array(listear)
-listear=(listear-min(listear))/(max(listear)-min(listear))
+listear=(listear-np.nanmin(listear))/(np.nanmax(listear)-np.nanmin(listear))
 listear=list(listear)
 
 col=['F1',"F2","F3","F4","F5",'F6',"F7","F8","F9","F10",'F11',"F12","F13"]
@@ -35,19 +35,4 @@ for i in range(6, len(listear)-7):
 	
 df_fin.index.name="frame"
 df_fin.dropna(how='any', inplace=True)
-
-df_fin = df_fin[df_fin.F1!=0]
-df_fin = df_fin[df_fin.F2!=0]
-df_fin = df_fin[df_fin.F3!=0]
-df_fin = df_fin[df_fin.F4!=0]
-df_fin = df_fin[df_fin.F5!=0]
-df_fin = df_fin[df_fin.F6!=0]
-df_fin = df_fin[df_fin.F7!=0]
-df_fin = df_fin[df_fin.F8!=0]
-df_fin = df_fin[df_fin.F9!=0]
-df_fin = df_fin[df_fin.F10!=0]
-df_fin = df_fin[df_fin.F11!=0]
-df_fin = df_fin[df_fin.F12!=0]
-df_fin = df_fin[df_fin.F13!=0]
-
 df_fin.to_csv("non_training_data_preproc/preproc_{}".format(args["data"][27:]), index=True, header=True)
