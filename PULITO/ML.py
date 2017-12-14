164be2bdb8abd9fcaab94046f5f8b1ee46123f59
rmenoli <30891698+rmenoli@users.noreply.github.com>
2017-12-14 11:56:59 +0100

    Pulizia

---
+++ PULITO/ML.py
@@ -0,0 +1,148 @@
+import numpy
+from pandas import DataFrame
+from matplotlib import pyplot
+from pandas import read_csv
+#from pandas import to_csv
+from pandas import set_option
+from pandas.plotting import scatter_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.metrics import roc_auc_score
+
+url = 'balanced_preproc/balanced_preproc_all.csv'
+dataset = read_csv(url, index_col="frame")
+
+print(dataset.shape)
+
+# descriptions, change precision to 3 places
+set_option('precision', 5)
+print(dataset.head())
+print(dataset.describe())
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,:dataset.shape[1]-1].astype(float)
+Y = array[:,dataset.shape[1]-1]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y,test_size=validation_size, random_state=seed)
+
+# Test options and evaluation metric
+num_folds = 10
+seed = 7
+scoring = 'accuracy'
+
+# Spot-Check Algorithms
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+results = []
+names = []
+for name, model in models:
+  kfold = KFold(n_splits=num_folds, random_state=seed)
+  cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+  results.append(cv_results)
+  names.append(name)
+  msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+  print(msg)
+
+# Compare Algorithms
+fig = pyplot.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+pyplot.show()
+
+# Standardize the dataset
+pipelines = []
+pipelines.append(('ScaledLR', Pipeline([('Scaler', StandardScaler()),('LR',
+LogisticRegression())])))
+pipelines.append(('ScaledLDA', Pipeline([('Scaler', StandardScaler()),('LDA',
+    LinearDiscriminantAnalysis())])))
+pipelines.append(('ScaledKNN', Pipeline([('Scaler', StandardScaler()),('KNN', KNeighborsClassifier())])))
+pipelines.append(('ScaledCART', Pipeline([('Scaler', StandardScaler()),('CART', DecisionTreeClassifier())])))
+pipelines.append(('ScaledNB', Pipeline([('Scaler', StandardScaler()),('NB', GaussianNB())])))
+pipelines.append(('ScaledSVM', Pipeline([('Scaler', StandardScaler()),('SVM', SVC())])))
+results = []
+names = []
+for name, model in pipelines:
+  kfold = KFold(n_splits=num_folds, random_state=seed)
+  cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+  results.append(cv_results)
+  names.append(name)
+  msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+  print(msg)
+
+
+# Compare Algorithms
+fig = pyplot.figure()
+fig.suptitle('Scaled Algorithm Comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+pyplot.show()
+
+# Tune scaled SVM
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+c_values = [0.1, 0.3, 0.5, 0.7, 0.9, 1.0, 1.3, 1.5, 1.7, 2.0]
+kernel_values = ['linear', 'poly', 'rbf', 'sigmoid']
+param_grid = dict(C=c_values, kernel=kernel_values)
+model = SVC()
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(rescaledX, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+# prepare the model
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+model = SVC(C=grid_result.best_params_['C'])  #choose our best model and C
+model.fit(rescaledX, Y_train)
+
+# estimate accuracy on validation dataset
+rescaledValidationX = scaler.transform(X_validation)
+predictions = model.predict(rescaledValidationX)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+
+print(roc_auc_score(Y_validation,predictions))
+
+def prev_to_csv(csv_in,scaler=scaler,model=model):
+    X = read_csv(csv_in, index_col="frame")
+    rescaledX = scaler.transform(X)
+    predictions = model.predict(rescaledX)
+    newdata = DataFrame(predictions, index=X.index, columns=["blink"])
+	return newdata
+
+previsioni=prev_to_csv("non_training_data_preproc/preproc_video_test_2.csv")
+privioni.to_csv("prova_validation.csv")
