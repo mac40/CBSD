14cf7701eb6d44c3a351a8f284da2c5c08a93c4f
andrea <andreadellavecchia94@gmail.com>
2017-12-18 16:33:54 +0100

    rate

---
+++ rate.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+
+url_data="xxx.csv"
+url_base="xxx.csv"
+dataset = pd.read_csv(url_data, index_col="frame")
+baseline = pd.read_csv(url_base, index_col="frame")
+#array di dati
+data=dataset.blink.values
+base=baseline.blink.values
+rate_medio=sum(base)/len(base)
+delta_frame=int(1/rate_medio)
+
+for i in range(len(data)):
+    if data[i]==1.0:
+        data[i]+=-1
+        #finestra separazione blink sinistra e lunga 2delta
+        for j in range(i-delta_frame+1,i+1):
+            data[j]+=1.0/(delta_frame)
+
+
+y=np.zeros(delta_frame)
+for k in range (delta_frame,len(data)):
+    y=np.append(y,sum(data[k-delta_frame:k])-1)
+
+newdata = pd.DataFrame(y, index=dataset.index, columns=["y"])
+newdata.to_csv("rate_prova.csv")
+
