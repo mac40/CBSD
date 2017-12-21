f0a9d06b9f0f07c2fe94783f0c9e262c42f6e297
andrea <andreadellavecchia94@gmail.com>
2017-12-21 18:35:13 +0100

    prova con finestre da 7

---
+++ PULITO/prova con finestre da 7/exe.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+title Sex Machine
+set /p _inputname= Please load your video in the "video" folder and input the name:
+echo Processing the video...
+python video_to_prevision.py -p shape_predictor_68_face_landmarks.dat -v video\%_inputname%
+@ECHO ON
+echo Creating frames...
+@ECHO OFF
+python .\show_video_EAR_NOFACE.py -v .\video\%_inputname% -q .\prevision\data_final_%_inputname:~0,-4%.csv
+echo Your video has been processed.
+set /p DUMMY= Hit enter to continue...
